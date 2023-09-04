db975df124d6288b26323b94b6eefdde2ddfc7fa
gabriel marucci <gabrielmarucci777@hotmail.com>
2023-09-04 10:58:16 -0300

    enviando projeto para deploy

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import "./NavBar.css";
+
+const NavBar = () => {
+  
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!search) return 
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  }
+
+  return (
+    <nav id="navbar">
+    <h2>
+      <Link to="/">
+        <BiCameraMovie /> MoviesLib</Link>
+    </h2>
+      <form onSubmit={handleSubmit} >
+        <input 
+        type="text" 
+        placeholder="Busque um filme" 
+        className="input_search" 
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
+
+        <button type="submit" className="btn_search" >
+          <BiSearchAlt2 />
+        </button>
+      </form>
+      
+    
+  </nav>
+  )
+}
+
+export default NavBar;
